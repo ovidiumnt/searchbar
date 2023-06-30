abcf17269fc29f0abf6966c7ee644eb51729eb06
Ovidiu P. Muntean <ovidiup.muntean@outlook.com>
2023-06-30 12:56:25 +0300

    Hotfixes

--- searchbar/TableViewController.swift
+++ searchbar/TableViewController.swift
@@ -56,12 +56,10 @@ class TableViewController: UITableViewController, UISearchResultsUpdating, UISea
             // MARK: - Table view data source
             
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
             
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return items.count
     }
             
